ed69fa9d0a1d0ccc0f861f8fc978a48d0b6ced9b
Ray Chu <raytanaka8@gmail.com>
2025-01-18 17:00:36 -0800

    changed to japanese

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let mainWindow = null
 function main() {
   mainWindow = new BrowserWindow()
   mainWindow.loadURL(`http://localhost:3000/`)
+  mainWindow.maximize()
   mainWindow.on('close', event => {
     mainWindow = null
   })
